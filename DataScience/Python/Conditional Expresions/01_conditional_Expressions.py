12c4dc5eb50e282cc4ebc9a2aff0aa479a353b5f
Ayush-Dobariya <ayushdobariya2024@gmail.com>
2025-11-15 16:13:03 +0530

    Add files via upload

---
+++ DataScience/Python/Conditional Expresions/01_conditional_Expressions.py
@@ -0,0 +1,15 @@
+# if else elif statements in python
+
+# a = 33 
+
+# if(a > 33):
+#     print("Hello world")
+# else:
+#     print("Niklja yaha se")
+
+a = int(input("Enter your age: "))
+
+if(a < 18):
+    print("You cannot vote now \nYou can't drive now")
+else:
+    print("You are able to vote now \nyou can drive now")
